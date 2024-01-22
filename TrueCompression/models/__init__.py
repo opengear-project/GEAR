f8a8f7db6b21f163643302af0311d8785f3a09e0
HaoKang-Timmy <466904389@qq.com>
2024-01-22 18:55:13 -0500

    mmlu for mistral

---
+++ TrueCompression/models/__init__.py
@@ -0,0 +1,9 @@
+from .modeling_gpt2 import GPT2LMHeadModel
+from .compress_config import GPT2CompressConfig
+from .modeling_opt import OPTForCausalLM, OPTConfig
+from .modeling_llama import LlamaForCausalLM
+from .modeling_llama_new import LlamaForCausalLMNew
+from .modeling_qwen import QWenLMHeadModel
+from .modeling_llama_h2o import H2OLlamaForCausalLM, LlamaConfig
+from .modeling_mistral import MistralForCausalLM, MistralConfig
+# from .modeling_mixtral import MixTralForCausalLM, MixtralConfig
