82a59f0a867d492412b81283a4496373f6a0e504
HaoKang-Timmy <466904389@qq.com>
2024-01-31 04:24:36 +0000

    zigzag schedule

--- TrueCompressionTest/models/__init__.py
+++ TrueCompressionTest/models/__init__.py
@@ -2,7 +2,7 @@
 from .compress_config import GPT2CompressConfig
 from .modeling_llama import LlamaForCausalLM
 from .modeling_llama_new import LlamaForCausalLMNew
-
+from .toy_model import ToyLlama
 # from .modeling_llama_h2o import H2OLlamaForCausalLM, LlamaConfig
 from .modeling_mistral import MistralForCausalLM, MistralConfig
 from .modify_llama_h2o import H2OLlamaForCausalLM, LlamaConfig
