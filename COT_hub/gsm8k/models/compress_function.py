840db7accf668402a4f71f72d7942ad08b543feb
souvikku <107592858+souvikku@users.noreply.github.com>
2024-02-13 08:41:51 -0800

    Update compress_function.py

--- COT_hub/gsm8k/models/compress_function.py
+++ COT_hub/gsm8k/models/compress_function.py
@@ -117,6 +117,53 @@ def fake_groupwise_asymmetric_quantization(input: torch.Tensor, quantize_bit):
     input = input.type(dtype)
     return input
 
+def fake_groupwise_token_asymmetric_quantization(input: torch.Tensor, quantize_bit, group_size=128):
+    batch, num_head, seq_len, sep_dim = input.shape
+    dtype = input.dtype
+    group_size = 128
+    input = (
+        input.permute(0, 2, 1, 3).contiguous().view(batch, seq_len, sep_dim * num_head)
+    ) 
+    num_groups = (sep_dim*num_head) // group_size
+    if num_groups * group_size != input.shape[-1]:
+        raise ValueError("group_size should be a factor of the last dimension size")
+
+    input_in_groups = input.view(batch, seq_len, num_groups, group_size)
+
+    mx, mn = input_in_groups.max(dim=-1)[0], input_in_groups.min(dim=-1)[0]
+    mx, mn = mx.unsqueeze(-1), mn.unsqueeze(-1)
+
+    scale = (mx - mn) / (2 ** quantize_bit - 1)
+    input_in_groups = (input_in_groups - mn) / scale
+    input_in_groups = F.relu(input_in_groups)
+    rounded_input_in_groups = input_in_groups.round_()
+    dequantized_input_in_groups = rounded_input_in_groups * scale + mn
+    dequantized_input = dequantized_input_in_groups.view(batch, seq_len, num_head, sep_dim)
+    dequantized_input = dequantized_input.permute(0, 2, 1, 3)
+    dequantized_input = dequantized_input.type(dtype)
+    return dequantized_input
+
+
+def fake_groupwise_channel_asymmetric_quantization(input: torch.Tensor, quantize_bit, group_size=128):
+    batch, num_head, seq_len, sep_dim = input.shape
+    dtype = input.dtype
+    group_size = 128
+    input = (
+        input.permute(0, 2, 1, 3).contiguous().view(batch, seq_len, sep_dim * num_head)
+    ) 
+
+    mx, mn = input.max(dim=-2)[0], input.min(dim=-2)[0]
+    mx, mn = mx.unsqueeze(-2), mn.unsqueeze(-2)
+
+    scale = (mx - mn) / (2 ** quantize_bit - 1)
+    input = (input - mn) / scale
+    input = F.relu(input)
+    rounded_input = input.round_()
+    dequantized_input = rounded_input * scale + mn
+    dequantized_input = dequantized_input.view(batch, seq_len, num_head, sep_dim)
+    dequantized_input = dequantized_input.permute(0, 2, 1, 3)
+    dequantized_input = dequantized_input.type(dtype)
+    return dequantized_input
 
 def fake_smoothquatization(input, quantize_bit):
     pass
@@ -550,6 +597,58 @@ def compress_insert_function(
                 previous_value[:, :, starting_idx:-locality_idx, :],
                 compress_config.quantize_bit[layer_idx],
             )
+            
+    if compress_config.compress_method[layer_idx] == "groupquantization_token":
+        previous_key[
+            :, :, starting_idx:-locality_idx, :
+        ] = fake_groupwise_token_asymmetric_quantization(
+            previous_key[:, :, starting_idx:-locality_idx, :],
+            compress_config.quantize_bit[layer_idx],
+            128
+        )
+        if previous_value is not None:
+            previous_value[
+                :, :, starting_idx:-locality_idx, :
+            ] = fake_groupwise_token_asymmetric_quantization(
+                previous_value[:, :, starting_idx:-locality_idx, :],
+                compress_config.quantize_bit[layer_idx],
+                128
+            )
+
+    if compress_config.compress_method[layer_idx] == "groupquantization_channel":
+        previous_key[
+            :, :, starting_idx:-locality_idx, :
+        ] = fake_groupwise_channel_asymmetric_quantization(
+            previous_key[:, :, starting_idx:-locality_idx, :],
+            compress_config.quantize_bit[layer_idx],
+            128
+        )
+        if previous_value is not None:
+            previous_value[
+                :, :, starting_idx:-locality_idx, :
+            ] = fake_groupwise_channel_asymmetric_quantization(
+                previous_value[:, :, starting_idx:-locality_idx, :],
+                compress_config.quantize_bit[layer_idx],
+                128
+            )
+
+    if compress_config.compress_method[layer_idx] == "groupquantization_kc_vt":
+        previous_key[
+            :, :, starting_idx:-locality_idx, :
+        ] = fake_groupwise_channel_asymmetric_quantization(
+            previous_key[:, :, starting_idx:-locality_idx, :],
+            compress_config.quantize_bit[layer_idx],
+            128
+        )
+        if previous_value is not None:
+            previous_value[
+                :, :, starting_idx:-locality_idx, :
+            ] = fake_groupwise_token_asymmetric_quantization(
+                previous_value[:, :, starting_idx:-locality_idx, :],
+                compress_config.quantize_bit[layer_idx],
+                128
+            )
+    
     if compress_config.compress_method[layer_idx] == "uniformquantization":
         # print("begin uniquant",starting_idx,locality_idx,compress_config.quantize_bit[layer_idx])
         previous_key[:, :, starting_idx:-locality_idx, :] = fake_uniformquantization(
